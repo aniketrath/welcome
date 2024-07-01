b285cf0b1f4416baf62dedfdbcb9b7284e237f18
Aniket Rath <aniketrath1121@gmail.com>
2024-07-01 14:39:39 +0530

    Removed Header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import {Introduction , Experience , Projects , Contact} from './components/index'
-import {Box, Header , Footer} from './components/Root/index'
+import {Box,  Footer} from './components/Root/index'
 export default function App() {
   return (
     <Box>
-    <Header/>
       <Introduction/>
       <Experience/>
       <Projects/>
       <Contact/>
-    <Footer/>
+      <Footer/>
     </Box>
   )
 }
